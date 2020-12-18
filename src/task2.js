f210a5a79bf84fbe26c0186d4ed53894a3758d4a
KotenkoNata <kotenkonata8@gmail.com>
2020-12-18 17:13:46 +0200

    Done second part of homework

---
+++ src/task2.js
@@ -0,0 +1,33 @@
+//Задание 2
+
+const users = [
+  { name: "Mango", active: true },
+  { name: "Poly", active: false },
+  { name: "Ajax", active: true },
+  { name: "Lux", active: false },
+];
+
+const toggleUserState = (allUsers, userName) => {
+  return new Promise(function (resolve, reject) {
+    const updatedUsers = allUsers.map((user) =>
+      user.name === userName ? { ...user, active: !user.active } : user
+    );
+
+    resolve(updatedUsers);
+  });
+};
+
+const logger2 = (updatedUsers) => console.table(updatedUsers);
+
+/*
+ * Сейчас работает так
+ */
+// toggleUserState(users, "Mango", logger);
+// toggleUserState(users, "Lux", logger);
+
+/*
+ * Должно работать так
+ */
+
+toggleUserState(users, "Mango").then(logger2);
+toggleUserState(users, "Lux").then(logger2);
